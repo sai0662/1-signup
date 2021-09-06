ea65d1df1f7effc106533719a6fe779b2eaad3e5
sai0662 <sait0662@gmail.com>
2021-09-06 23:56:58 +0530

    sign up page

---
+++ signup.js
@@ -0,0 +1,8 @@
+function signup()
+{
+    /*var str = document.getElementById("firstname").value;
+    var str2 = document.getElementById("lastname").value;
+    var str2 = document.getElementById("mailid").value;*/
+    alert("Successfully signup");
+
+}
